6b9251248b6c41a7e645f89905b4207374a6905b
Glen Mailer <glen@circleci.com>
2019-09-22 13:42:47 +0100

    Draw the rest of the owl

--- trigger-pipeline/rollup.config.js
+++ trigger-pipeline/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
 
 export default {
   input: "./src/main.ts",
@@ -9,7 +10,12 @@ export default {
     format: "cjs"
   },
 
-  plugins: [resolve({ preferBuiltins: true }), commonjs(), typescript()],
+  plugins: [
+    resolve({ preferBuiltins: true }),
+    commonjs(),
+    json(),
+    typescript()
+  ],
 
   external: ["path", "os"]
 };
--- trigger-pipeline/src/main.ts
+++ trigger-pipeline/src/main.ts
@@ -1,13 +1,103 @@
 import * as core from "@actions/core";
+import dotprop from "dot-prop";
+import httpism from "httpism";
+
+type Args = {
+  token: string;
+  vcs: string;
+  org: string;
+  repo: string;
+  branch: string;
+  parameters: object;
+};
+
+type TriggerResponse = {
+  number: number;
+  id: string;
+  created_at: string;
+};
 
 async function run() {
   try {
-    const myInput = core.getInput("org");
-    core.debug(`Hello ${myInput}`);
-    core.debug("env: " + JSON.stringify(process.env, null, 2));
+    const args = readArgs();
+
+    console.log("Triggering CircleCI Pipeline with details:");
+    console.dir(args, { color: true }); // rely on GitHub to mask token secret
+
+    const resp = await postRequest(args);
+
+    core.setOutput("pipeline_number", String(resp.number));
+    core.setOutput("pipeline_id", resp.id);
+
+    console.log("CircleCI Pipeline Created");
+    console.dir(resp, { color: true });
   } catch (ex) {
     core.setFailed(ex.message);
   }
+}
+
+function readArgs(): Args {
+  try {
+    const token = core.getInput("token", { required: true });
+    const vcs = core.getInput("vcs") || "github";
+    const org = readArg("org", "organization.login");
+    const repo = readArg("repo", "repository.name");
+    const branch = readArg("repo", "pull_request.head.ref");
+    const parameters = JSON.parse(core.getInput("parameters_json") || "{}");
+
+    return { token, vcs, org, repo, branch, parameters };
+  } catch (ex) {
+    console.log("Failed to read args");
+    console.dir(process.env, { depth: Infinity, color: true });
+    console.dir(getEvent(), { depth: Infinity, color: true });
+    throw ex;
+  }
+}
+
+async function postRequest(args: Args): Promise<TriggerResponse> {
+  const client = httpism.client("https://circleci.com/api/v2/", {
+    headers: {
+      "user-agent": "trigger-pipeline GitHub Action",
+      "Circle-Token": args.token
+    },
+    responseBody: "json"
+  });
+
+  const resp = await client.post(
+    "project/:vcs/:org/:project/pipeline",
+    {
+      branch: args.branch,
+      parameters: args.parameters
+    },
+    {
+      params: {
+        vcs: args.vcs,
+        org: args.org,
+        project: args.repo
+      }
+    }
+  );
+
+  return resp;
+}
+
+function readArg(input: string, fallback: string): string {
+  const val = core.getInput(input);
+  if (val) {
+    return val;
+  }
+  const event = getEvent();
+  if (dotprop.has(event, fallback)) {
+    return dotprop.get<string>(event, fallback);
+  }
+  throw new Error(`Missing '${input}' argument`);
+}
+
+function getEvent(): object {
+  if (process.env.GITHUB_EVENT_PATH) {
+    return require(process.env.GITHUB_EVENT_PATH);
+  }
+  return {};
 }
 
 run();
